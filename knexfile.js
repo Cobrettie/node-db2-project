f2bb7f0288c9be81fbe1b942ca24f4af4c55110b
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-18 23:48:12 -0400

    update knexfile.js + install sqlite3

--- knexfile.js
+++ knexfile.js
@@ -5,39 +5,12 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './data/lambda.db3',
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
+    // generates migration files in a data/migrations/ folder
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
     }
   }
 
