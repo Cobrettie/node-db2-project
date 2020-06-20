53ea84e02c7bc0e8cabf6ab8417ddaf23432e35d
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-19 21:14:02 -0400

    configure endpoint to post new zoo

--- zoosRouter.js
+++ zoosRouter.js
@@ -1,20 +1,36 @@
 const express = require('express');
 
-const zoosDB = require('./data/dbConfig');
+const knex = require('./data/dbConfig');
 
 const router = express.Router();
 
 // endpoints here
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({
+      errorMessage: 'Name field required'
+    })
+  }
+  
+  try {
+    await knex('zoos').insert(req.body)
+      .then(addedZooId => {
+        res.status(201).json(addedZooId)
+      })
+  } catch (err) {
+    console.log('Error: ', err);
+    res.status(500).json({
+      errorMessage: 'Could not add zoo'
+    })
+  }
 })
 
 router.get('/', async (req, res) => {
   try {
-    const zoos = await zoosDB('zoos')
+    const zoos = await knex('zoos')
     res.status(200).json(zoos)
-  } catch {
+  } catch (err) {
     console.log('Error: ', err);
     res.status(500).json({
       errorMessage: 'Failed to retrieve zoos'
@@ -22,8 +38,24 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.get('/:id', (req, res) => {
-  
+router.get('/:id', async (req, res) => {
+
+  try {
+  const {id} = req.params;
+    const zoo = await knex('zoos').where({id: id}).first()
+    if (id) {
+      res.status(201).json(zoo)
+    } else {
+      res.status(400).json({
+        errorMessage: "Invalid ID"
+      })
+    }
+  } catch (err) {
+    console.log('Error: ', err);
+    res.status(500).json({
+      errorMessage: 'Could not retrieve zoo'
+    })
+  }
 })
 
 router.put('/:id', (req, res) => {
